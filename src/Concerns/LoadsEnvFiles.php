e584f3444fa91b4f80538d63d64d6705559daae8
Sergio Compean <scompean24@gmail.com>
2022-10-22 17:38:27 -0400

    add parse method for env trait

--- src/Concerns/LoadsEnvFiles.php
+++ src/Concerns/LoadsEnvFiles.php
@@ -8,6 +8,20 @@ use InvalidArgumentException;
 trait LoadsEnvFiles
 {
     /**
+     * Loads a env file and returns parsed data as an array.
+     * The variables are NOT loaded into $_ENV global, simply
+     * parsed from file and returned.
+     */
+    public function getEnvFileVariables(string $path): array
+    {
+        if (! is_file($path)) {
+            throw new InvalidArgumentException("The env file '$path' file does not exist.");
+        }
+
+        return Dotenv::parse(file_get_contents($path));
+    }
+
+    /**
      * Loads a env file variables into $_ENV and return the result.
      */
     public function loadEnvFileVariables(string $path): array
