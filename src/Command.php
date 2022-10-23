055c69c26771d28f6f127b020b27ffc22b3d4132
Sergio Compean <scompean24@gmail.com>
2022-10-23 12:16:02 -0400

    check if pcntl is loaded

--- src/Command.php
+++ src/Command.php
@@ -24,7 +24,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 use function Termwind\render;
 use function Termwind\renderUsing;
-use function Termwind\{ask};
 
 if (class_exists(LaravelZeroCommand::class)) {
     abstract class BaseCommand extends LaravelZeroCommand
@@ -60,7 +59,7 @@ abstract class Command extends BaseCommand
 
     public function __set(string $name, mixed $value)
     {
-        // prevent depecration notice in 8.2.
+        // prevent depecration notice in 8.2 for dynamic properties.
     }
 
     /**Get the console components instance.*/
@@ -110,8 +109,11 @@ abstract class Command extends BaseCommand
     }
 
     /**Run a new command task.*/
-    public function runTask(string $title = '', $task = null): CommandTask
+    public function runTask(string $title = '', $task = null)
     {
+        if (! extension_loaded('pcntl')) {
+            return $this->task($title, $task);
+        }
         $task = (new CommandTask($title, $this, $task));
 
         $task->run();
@@ -275,7 +277,9 @@ abstract class Command extends BaseCommand
         // merge the data together.
         $data = (new DataTransformer(array_merge($this->arguments(), $this->options()), ['*date*' => ['?', Carbon::class]]))->transform();
 
-        $this->data = collect($data);
+        $this->data = collect($data)->filter(function ($v) {
+            return ! is_null($v);
+        });
 
         // transform the data before validation
         if ($withTransformers = $this->classUsesTrait($this, WithTransformers::class) && $transformers = $this->transformers()) {
